3de438f3200262c5eaff1dfb644f8470224c75a3
Joachim <joachim.berner@hotmail.com>
2024-09-16 20:18:51 +0200

    Updates and installed new packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Navbar from '../src/Components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl text-sky-400/75 font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Navbar />
+      <h1 className="text-3xl text-sky-400/75 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   );
 }
 
