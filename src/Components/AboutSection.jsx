9b48b5dd8f65f813dcd2dac50c46fe34bcf37ded
Joachim <joachim.berner@hotmail.com>
2024-09-27 02:55:45 +0200

    Updated the Aboutsection with the new custom styling from Tailwind, and some smaller content changes

--- src/Components/AboutSection.jsx
+++ src/Components/AboutSection.jsx
@@ -7,12 +7,12 @@ function AboutSection() {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center justify-between space-y-8 md:space-y-0">
         {/* Text Section */}
         <div className="md:w-1/2 w-full text-left md:pr-8">
-          <h4 className="text-3xl font-heading font-medium text-regal-purple mb-4 uppercase">
+          <h4 className="text-3xl font-heading font-medium text-secondary-color mb-4 uppercase">
             Om Mig
           </h4>
           <h1 className="text-5xl font-heading font-bold text-black-700 mb-8 leading-tight">
             Helping businesses deliver{' '}
-            <span className="text-regal-purple">exceptional</span> buyer experiences.
+            <span className="text-secondary-color">exceptional</span> buyer experiences.
           </h1>
           <p className="font-text text-gray-600 mb-8 leading-relaxed">
             Vidyard is the leading video messaging and asynchronous communications platform for go-to-market teams. Millions of sales professionals and more than 250,000 go-to-market teams use Vidyard’s AI-powered video messaging, video hosting, and digital sales rooms to connect with more prospects and generate more revenue.
